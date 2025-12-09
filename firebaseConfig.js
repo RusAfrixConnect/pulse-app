5f7c225a089f43567d53d9bb155ab9a750d70265
RusAfrixConnect <dabaran75@gmail.com>
2025-12-09 20:40:55 +0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+// Configuration Firebase (à remplir plus tard)
+const firebaseConfig = {
+  apiKey: "À_REMPLIR",
+  authDomain: "À_REMPLIR",
+  projectId: "À_REMPLIR",
+  storageBucket: "À_REMPLIR",
+  messagingSenderId: "À_REMPLIR",
+  appId: "À_REMPLIR"
+};
+
+export default firebaseConfig;
