1309272002227633cc7d40352663b19ede084b70
RusAfrixConnect <dabaran75@gmail.com>
2025-12-11 22:20:36 +0300

    Update MapScreen.js

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -10,7 +10,7 @@ import {
   ActivityIndicator 
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { db } from '../../firebaseConfig';
+import { db } from '../firebaseConfig';
 import { collection, onSnapshot } from 'firebase/firestore';
 
 export default function MapScreen() {
